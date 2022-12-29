f183ec914fcb34c914779b805747d96ee7f7146a
shreecreation <106921185+shreecreation@users.noreply.github.com>
2022-12-29 13:55:08 -0500

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,7 @@
 /**
  * Created by jt on 2018-11-26.
  */
-public class HelloWorldd {
+public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!! ");
